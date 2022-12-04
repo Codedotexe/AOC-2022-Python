ecb62d295c03888f6560dc508922eeb347307280
Tom <Tom>
2022-12-04 23:36:29 +0100

    Changed build setup

--- src/cli.py
+++ src/cli.py
@@ -1,53 +1,61 @@
+import importlib
 import logging
-import os
-import runpy
-import sys
+import aocd
 from argparse import ArgumentParser
 from datetime import datetime
-from shutil import copy
-
-from aocd.get import most_recent_year
-from aocd.models import Puzzle
-from aocd.utils import AOC_TZ
 
 
-def run_one():
+def _createParser():
 	parser = ArgumentParser(description=__doc__)
-	aoc_now = datetime.now(tz=AOC_TZ)
-	parser.add_argument(
-		"day",
-		nargs="?",
-		type=int,
-		choices=range(1, 26),
-		default=min(aoc_now.day, 25) if aoc_now.month == 12 else 1,
-		help="1-25 (default: %(default)s)",
-	)
-	parser.add_argument(
-		"year",
-		nargs="?",
-		type=int,
-		choices=range(2015, aoc_now.year + int(aoc_now.month == 12)),
-		default=most_recent_year(),
-		help=">= 2015 (default: %(default)s)",
-	)
+	aoc_now = datetime.now(tz=aocd.utils.AOC_TZ)
+	dayChoices = list(map(str, range(1, 26))) + ["all"]
+
+	parser.add_argument("day", nargs="?", type=str, choices=dayChoices,
+						default=str(min(aoc_now.day, 25)) if aoc_now.month == 12 else "1",
+						help="1-25 or all (default: %(default)s)")
 	parser.add_argument("-d", "--data")
 	log_levels = "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"
 	parser.add_argument("--log-level", choices=log_levels)
+	return parser
+
+
+def _runSolution(day, data=None):
+	modName = f"src.Day{day:02d}.main"
+	mod = importlib.import_module(modName)
+	if data is None:
+		data = aocd.get_data(day=day, year=2022)
+	solution = mod.Solution(data)
+	print(f"Day-{day:02d} Part 1:", solution.part1())
+	print(f"Day-{day:02d} Part 2:", solution.part2())
+
+
+def runOne():
+	parser = _createParser()
 	args = parser.parse_args()
 	if args.log_level:
 		level_int = getattr(logging, args.log_level)
 		logging.basicConfig(format="%(message)s", level=level_int)
-	mod_name = f"aoc_wim.aoc{args.year}.q{args.day:02d}"
-	sys.modules.pop(mod_name, None)
-	if args.data is not None:
-		os.environ["AOC_SESSION"] = "aocw"
-		path = os.path.expanduser("~/.config/aocd/aocw/")
-		os.makedirs(path, exist_ok=True)
-		path += f"{args.year}_{args.day:02d}_input.txt"
-		if os.path.isfile(args.data):
-			copy(args.data, path)
-		else:
-			with open(path, "w") as f:
-				print(args.data, file=f)
-	print(f"--- {args.year} Day {args.day}: {Puzzle(args.year, args.day).title} ---")
-	runpy.run_module(mod_name, run_name="__main__")
+
+	if args.data is not None and args.day == "all":
+		raise RuntimeError()
+
+	if args.day == "all":
+		for i in range(1, 26):
+			try:
+				_runSolution(i)
+			except ModuleNotFoundError:
+				break
+	else:
+		data = None
+		if args.data is not None:
+			with open(args.data, "r") as file:
+				data = file.read()
+
+		try:
+			_runSolution(int(args.day), data)
+		except ModuleNotFoundError:
+			print(f"The solution for day {args.day} is not yet implemented.")
+
+
+if __name__ == "__main__":
+	runOne()
