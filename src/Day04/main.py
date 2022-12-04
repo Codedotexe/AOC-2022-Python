33a9fa6abf07865eda0fb4cfb5590d34d5cd17d9
Tom <Tom>
2022-12-04 21:00:44 +0100

    Added solution for day 4

---
+++ src/Day04/main.py
@@ -0,0 +1,75 @@
+from src.utils.PuzzleSolution import PuzzleSolution
+import aocd
+import re
+
+class Solution(PuzzleSolution):
+	pairs = []
+
+	class Pair:
+		aStart = None
+		aEnd = None
+		bStart = None
+		bEnd = None
+
+		def __init__(self, aStart: int, aEnd: int, bStart: int, bEnd: int) -> None:
+			self.aStart = aStart
+			self.aEnd = aEnd
+			self.bStart = bStart
+			self.bEnd = bEnd
+
+		def encloses(self) -> bool:
+			if self.aStart >= self.bStart and self.aEnd <= self.bEnd:  # b encloses a
+				return True
+			elif self.aStart <= self.bStart and self.aEnd >= self.bEnd:  # a encloses b
+				return True
+			else:
+				return False
+
+		def overlaps(self) -> bool:
+			if self.aStart <= self.bStart <= self.aEnd:
+				return True
+			elif self.aStart <= self.bEnd <= self.aEnd:
+				return True
+			elif self.bStart <= self.aStart <= self.bEnd:
+				return True
+			elif self.bStart <= self.aEnd <= self.bEnd:
+				return True
+			else:
+				return False
+
+		def __repr__(self):
+			return f"{self.aStart}-{self.aEnd},{self.bStart}-{self.bEnd}"
+
+	def __init__(self, data: str) -> None:
+		data = data.rstrip("\n").split("\n")
+		for entryStr in data:
+			match = re.search("^(\d+)-(\d+),(\d+)-(\d+)$", entryStr)
+			assert(match is not None)
+			self.pairs.append(self.Pair(
+				int(match.group(1)),
+				int(match.group(2)),
+				int(match.group(3)),
+				int(match.group(4))
+			))
+
+		assert(len(self.pairs) > 1)
+
+	def part1(self) -> int:
+		count = 0
+		for pair in self.pairs:
+			if pair.encloses():
+				count += 1
+		return count
+
+	def part2(self):
+		count = 0
+		for pair in self.pairs:
+			if pair.overlaps():
+				count += 1
+		return count
+
+
+if __name__ == "__main__":
+	solution = Solution(aocd.get_data(day=4, year=2022))
+	print(solution.part1())
+	print(solution.part2())
---
+++ src/Day04/test.py
@@ -0,0 +1,27 @@
+import unittest
+from src.Day04.main import Solution
+
+class TestDay04(unittest.TestCase):
+	solution = None
+
+	@classmethod
+	def setUpClass(self) -> None:
+		data = "\n".join([
+			"2-4,6-8",
+			"2-3,4-5",
+			"5-7,7-9",
+			"2-8,3-7",
+			"6-6,4-6",
+			"2-6,4-8"
+		])
+		self.solution = Solution(data)
+
+	def testPart1(self):
+		self.assertEqual(2, self.solution.part1())
+
+	def testPart2(self):
+		self.assertEqual(4, self.solution.part2())
+
+
+if __name__ == '__main__':
+	unittest.main()
